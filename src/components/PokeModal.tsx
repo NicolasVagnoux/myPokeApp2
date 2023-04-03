e1a90c9c3a99320a8222de06843481cf3a7ee36b
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2023-04-03 17:49:29 +0200

    closing modal + css update

--- src/components/PokeModal.tsx
+++ src/components/PokeModal.tsx
@@ -109,41 +109,44 @@ const PokeModal = ({ id, setIsModalOpened, location }: Props) => {
     return (
         <>
         {info &&
-        <div className='pokeModal'>
-            <div className='pokeModal__content'>
-                <div className='pokeModal__content__1st' style={{backgroundColor: `${selectedType.color}90`}}>
-                    <button className='pokeModal__content__1st__close' type='button' onClick={() => setIsModalOpened(false)}><img src="./assets/cross.svg" alt="close" /></button>
-                    <div className='pokeModal__content__1st__title'>
-                        <h2>{info.name}</h2>
-                        <div>
-                            <button type='button' onClick={isInTheTeam ? removePokeFromTheTeam : addPokeToTheTeam}><img src={`${isInTheTeam ? './assets/remove.svg' : './assets/add.svg'}`} title={`${isInTheTeam ? 'Retirer de mon équipe' : 'Ajouter à mon équipe'}`} alt='add/remove' /></button>
-                            <h3>#{info.id < 10 ? '00'+info.id : info.id < 100 ? '0'+info.id : info.id}</h3>
+        <>
+            <div className='background' onClick={() => setIsModalOpened(false)}></div>
+            <div className='pokeModal'>
+                <div className='pokeModal__content'>
+                    <div className='pokeModal__content__1st' style={{backgroundColor: `${selectedType.color}90`}}>
+                        <button className='pokeModal__content__1st__close' type='button' onClick={() => setIsModalOpened(false)}><img src="./assets/cross.svg" alt="close" /></button>
+                        <div className='pokeModal__content__1st__title'>
+                            <h2>{info.name}</h2>
+                            <div>
+                                <button type='button' onClick={isInTheTeam ? removePokeFromTheTeam : addPokeToTheTeam}><img src={`${isInTheTeam ? './assets/remove.svg' : './assets/add.svg'}`} title={`${isInTheTeam ? 'Retirer de mon équipe' : 'Ajouter à mon équipe'}`} alt='add/remove' /></button>
+                                <h3>#{info.id < 10 ? '00'+info.id : info.id < 100 ? '0'+info.id : info.id}</h3>
+                            </div>
                         </div>
-                    </div>
-                    <div className='pokeModal__content__1st__types'>
-                        <h3>{info.apiTypes[0].name}</h3>
-                        <h3>{info.apiTypes[1] && info.apiTypes[1].name}</h3>
-                    </div>
-                    <img className='pokeModal__content__1st__image' src={info.image} alt={info.name} />
-                </div>
-                <div className='pokeModal__content__2nd'>
-                    <div className='pokeModal__content__2nd__arrows'>
-                        <button onClick={decreaseId}><img src='./assets/left-arrow.svg' alt='left-arrow' /></button>
-                        <button onClick={increaseId}><img src='./assets/left-arrow.svg' alt='right-arrow' /></button>
-                    </div>
-                    <div className='pokeModal__content__2nd__strat'>
-                        <div className='pokeModal__content__2nd__strat__stats'>
-                            <p>Statistiques :</p>
-                            <StatChart {...info} />
+                        <div className='pokeModal__content__1st__types'>
+                            <h3>{info.apiTypes[0].name}</h3>
+                            <h3>{info.apiTypes[1] && info.apiTypes[1].name}</h3>
                         </div>
-                        <div className='pokeModal__content__2nd__strat__resistances'>
-                            <p>Sensibilités :</p>
-                            <DefensiveChart {...info} />
+                        <img className='pokeModal__content__1st__image' src={info.image} alt={info.name} />
+                    </div>
+                    <div className='pokeModal__content__2nd'>
+                        <div className='pokeModal__content__2nd__arrows'>
+                            <button onClick={decreaseId}><img src='./assets/left-arrow.svg' alt='left-arrow' /></button>
+                            <button onClick={increaseId}><img src='./assets/left-arrow.svg' alt='right-arrow' /></button>
+                        </div>
+                        <div className='pokeModal__content__2nd__strat'>
+                            <div className='pokeModal__content__2nd__strat__stats'>
+                                <p>Statistiques :</p>
+                                <StatChart {...info} />
+                            </div>
+                            <div className='pokeModal__content__2nd__strat__resistances'>
+                                <p>Sensibilités :</p>
+                                <DefensiveChart {...info} />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
         }
         </>
     );
